bba3c80c874bc1e1c1e9454a6a4e9f8420cd47a6
Marcus Edwards <marcusedwards@hotmail.ca>
2017-04-06 18:30:12 -0400

    add content arrow

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -126,7 +126,7 @@
                         <a href="https://github.com/MackEdweise/scapi" class="btn btn-lg btn-social-icon btn-github">
                             <span class="fa fa-github"></span>
                         </a>
-                        <h3 class="spacer-2">Add Content</h3>
+                        <h3 class="spacer-2 text-center">Add Content</h3>
                         <span class="glyphicon glyphicon-arrow-down"></span>
                     </div>
                 </div>
