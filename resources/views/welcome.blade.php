a3b9aa7b2956b22a227f3e29128315849931239e
Marcus Edwards <marcusedwards@hotmail.ca>
2017-04-07 16:49:30 -0400

    github button style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -124,7 +124,7 @@
                 </div>
             </div>
             <div class="row">
-                <a href="https://github.com/MackEdweise/scapi" class="btn btn-lg btn-social btn-block btn-github" style="width:260px;margin-left:auto;margin-right:auto;">
+                <a href="https://github.com/MackEdweise/scapi" class="btn btn-lg btn-social btn-block btn-github" style="width:260px;margin-left:auto;margin-right:auto;margin-top:50px;margin-bottom:25px;">
                     <span class="fa fa-github"></span> Fork the Code
                 </a>
             </div>
