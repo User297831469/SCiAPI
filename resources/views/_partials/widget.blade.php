7a40266297f094ea248cc13278a7fc8bc051742e
Marcus Edwards <marcusedwards@hotmail.ca>
2017-04-08 21:53:04 -0400

    update partial for rendering widgets

--- resources/views/_partials/widget.blade.php
+++ resources/views/_partials/widget.blade.php
@@ -1,8 +1,10 @@
 <div class="col-md-4 col-sm-6 hero-feature">
     <div class="thumbnail">
-        <img src="{{ 'http://23.248.66.120:9090/'.$widget->image }}" alt="heat sink">
+        <img src="{{ 'http://23.248.66.120:9090/'.$widget->image }}" alt="image">
         <div class="caption">
-            <h3>{{ $widget->name }}</h3>
+            <div class="row">
+                <h3 class="subtitle">{{ $widget->name }}</h3><a class='my-tool-tip' data-toggle="tooltip" data-placement="top" title="Want to edit this because you made a mistake or think something isn't right? Let us know why and we can help." href="mailto:marcusedwards@hotmail.ca"><i class="fa fa-pencil-square fa-fw"></i></a>
+            </div>
             <ul class="nav nav-tabs">
                 <li class="active"><a data-toggle="tab" href="#home-{{ $widget->id }}"><img class="widget-tab" src="img/path4200.png" alt="f(x)"></a></li>
                 <li><a data-toggle="tab" href="#menu-{{ $widget->id }}-2"><img class="widget-tab" src="img/logo-JavaScript.png" alt="JS"></a></a></li>
@@ -20,12 +22,14 @@
                     </div>
                 </div>
                 <div id="menu-{{ $widget->id }}-2" class="tab-pane fade text-left">
-                    <pre>
-                        {{ $widget->code }}
-                    </pre>
+                                        <pre>
+                                            @foreach(explode("\n", $widget->code) as $line)
+                                                <span>{{ $line  }}</span>
+                                            @endforeach
+                                        </pre>
                 </div>
                 <div id="menu-{{ $widget->id }}-3" class="tab-pane fade wolfram">
-                    {{ $widget->wolfram }}
+                    {!! html_entity_decode($widget->wolfram) !!}
                 </div>
             </div>
         </div>
