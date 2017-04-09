d7177493fd13de7c657dd8f23f9ae8b45ae97cb2
Marcus Edwards <marcusedwards@hotmail.ca>
2017-04-08 20:40:52 -0400

    tooltip placement

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -296,37 +296,37 @@
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="row">
-                                            <h4><b>Name</b></h4><span><a class='my-tool-tip' data-toggle="tooltip" data-placement="top" title="Name the Computation."><i class='glyphicon glyphicon-info-sign'></i></a></span>
+                                            <span><h4><b>Name</b></h4></span><span><a class='my-tool-tip' data-toggle="tooltip" data-placement="top" title="Name the Computation."><i class='glyphicon glyphicon-info-sign'></i></a></span>
                                         </div>
                                         <div class="row">
                                             <input required id="name" name="name" type="text" class="form-control" placeholder="Capacitance">
                                         </div>
                                         <div class="row">
-                                            <h4><b>Description</b></h4><span><a class='my-tool-tip' data-toggle="tooltip" data-placement="top" title="Describe the formula. Note its parameters."><i class='glyphicon glyphicon-info-sign'></i></a></span>
+                                            <span><h4><b>Description</b></h4></span><span><a class='my-tool-tip' data-toggle="tooltip" data-placement="top" title="Describe the formula. Note its parameters."><i class='glyphicon glyphicon-info-sign'></i></a></span>
                                         </div>
                                         <div class="row">
                                             <textarea value="" required id="description" name="description" type="text" class="form-control " placeholder="This is a known relationship from physics, chemistry, biology, astronomy, electronics, etc."></textarea>
                                         </div>
                                         <div class="row">
-                                            <h4><b>JavaScript Code</b></h4><span><a class='my-tool-tip' data-toggle="tooltip" data-placement="top" title="Please use unique, descriptive parameter names for your function and best coding practices. Please make any necessary explanations in the description. If you don't know what to put here, please enter '//TODO - need a dev!'"><i class='glyphicon glyphicon-info-sign'></i></a></span>
+                                            <span><h4><b>JavaScript Code</b></h4></span><span><a class='my-tool-tip' data-toggle="tooltip" data-placement="top" title="Please use unique, descriptive parameter names for your function and best coding practices. Please make any necessary explanations in the description. If you don't know what to put here, please enter '//TODO - need a dev!'"><i class='glyphicon glyphicon-info-sign'></i></a></span>
                                         </div>
                                         <div class="row">
                                             <textarea value="" required id="code" name="code" type="text" class="form-control " placeholder="function myFormula(){"></textarea>
                                         </div>
                                         <div class="row">
-                                            <h4><b>Wolfram Alpha Widget</b></h4><span><a class='my-tool-tip' data-toggle="tooltip" data-placement="top" title="In order to obtain the wolfram alpha widget link, please create your widget by following Wolfram Alpha's instructions (developer.wolframalpha.com/widgetbuilder). At the end, paste the embed link with the 'popup' option into this field."><i class='glyphicon glyphicon-info-sign'></i></a></span>
+                                            <span><h4><b>Wolfram Alpha Widget</b></h4></span><span><a class='my-tool-tip' data-toggle="tooltip" data-placement="top" title="In order to obtain the wolfram alpha widget link, please create your widget by following Wolfram Alpha's instructions (developer.wolframalpha.com/widgetbuilder). At the end, paste the embed link with the 'popup' option into this field."><i class='glyphicon glyphicon-info-sign'></i></a></span>
                                         </div>
                                         <div class="row">
                                             <input required id="wolfram" name="wolfram" type="text" class="form-control " placeholder="<script>...</script>">
                                         </div>
                                         <div class="row">
-                                            <h4><b>Related Image</b></h4><span><a class='my-tool-tip' data-toggle="tooltip" data-placement="top" title="Ideally a picture that relates to the science!"><i class='glyphicon glyphicon-info-sign'></i></a></span>
+                                            <span><h4><b>Related Image</b></h4></span><span><a class='my-tool-tip' data-toggle="tooltip" data-placement="top" title="Ideally a picture that relates to the science!"><i class='glyphicon glyphicon-info-sign'></i></a></span>
                                         </div>
                                         <div class="row">
                                             <input type="file" name="image" id="image" size="20" />
                                         </div>
                                         <div class="row">
-                                            <h4><b>Formula Image</b></h4><span><a class='my-tool-tip' data-toggle="tooltip" data-placement="top" title="Please upload an image of the formula in readable text."><i class='glyphicon glyphicon-info-sign'></i></a></span>
+                                            <span><h4><b>Formula Image</b></h4></span><span><a class='my-tool-tip' data-toggle="tooltip" data-placement="top" title="Please upload an image of the formula in readable text."><i class='glyphicon glyphicon-info-sign'></i></a></span>
                                         </div>
                                         <div class="row">
                                             <input type="file" name="formula" id="formula" size="20" />
