c725a88a8edf5ba94130c305b8289f9128147a46
Marcus Edwards <marcusedwards@hotmail.ca>
2017-04-10 13:10:38 -0400

    include user in widget controller

--- app/Http/Controllers/WidgetController.php
+++ app/Http/Controllers/WidgetController.php
@@ -11,6 +11,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\Widgets;
+use App\User;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
