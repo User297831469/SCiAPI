c91868ae87ec343ce3b4d43b37e5b62c92490654
Marcus Edwards <marcusedwards@hotmail.ca>
2017-04-10 14:30:13 -0400

    api key clippy centering

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,19 +84,17 @@
                 </div>
             </div>
             <div class="row text-center centered">
-                <div class="col-md-offset-3 col-sm-offset-3 col-lg-offset-3 col-xs-offset-1 col-md-6 col-sm-6 col-lg-6 col-xs-10 centered">
-                    @if (!is_null($user))
-                        <h4>Your API Key is:</h4>
-                        <div class="input-group">
-                            <input id="clipboard-target" class="form-control" value="{{ $user->key }}" readonly>
-                            <span data-toggle="tooltip" title="Copied!" class="input-group-addon btn btn-info clip-button" data-clipboard-target="#clipboard-target">
-                                <img class="clippy" src="{{ URL::asset('img/clippy.svg') }}" width="15">
-                            </span>
-                        </div>
-                    @else
-                        <a class="btn btn-lg btn-success" href="{{ route('register') }}">Get an API Key</a>
-                    @endif
-                </div>
+                @if (!is_null($user))
+                    <h4 class="center-block">Your API Key is:</h4>
+                    <div class="input-group center-block">
+                        <input id="clipboard-target" class="form-control" value="{{ $user->key }}" readonly>
+                        <span data-toggle="tooltip" title="Copied!" class="input-group-addon btn btn-info clip-button" data-clipboard-target="#clipboard-target">
+                            <img class="clippy" src="{{ URL::asset('img/clippy.svg') }}" width="15">
+                        </span>
+                    </div>
+                @else
+                    <a class="btn btn-lg btn-success" href="{{ route('register') }}">Get an API Key</a>
+                @endif
             </div>
             <div class="row">
                 <hr>
