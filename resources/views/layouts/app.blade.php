09f693531250557763ee589b0e4afe82d5b7185e
Marcus Edwards <marcusedwards@hotmail.ca>
2017-04-10 13:03:33 -0400

    logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,9 +60,19 @@
                             <li>
                                 <a href="#widgets">Widgets</a>
                             </li>
-                            <li>
-                                <a href="{{ route('login') }}">Login</a>
-                            </li>
+                            @if(is_null($user))
+                                <li>
+                                    <a href="{{ route('login') }}">Login</a>
+                                </li>
+                            @else
+                                <li>
+                                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            @endif
                         </ul>
                     </div>
                     <!-- /.navbar-collapse -->
