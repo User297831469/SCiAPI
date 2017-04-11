b2f95f123891be0ca98270f63095426a8e2120d9
Marcus Edwards <marcusedwards@hotmail.ca>
2017-04-10 22:18:16 -0400

    editabel widgets

--- resources/views/_partials/editableWidget.blade.php
+++ resources/views/_partials/editableWidget.blade.php
@@ -43,7 +43,7 @@
 <div id="updateModal-{{ $widget->id }}" class="modal fade" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">
+            <div class="modal-header text-left">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <h4 class="modal-title">Edit this Computation</h4>
             </div>
