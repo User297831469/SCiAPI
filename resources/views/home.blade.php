c67ff9c785fc1502ed02d564c9564e8f86b4705f
Marcus Edwards <marcusedwards@hotmail.ca>
2017-11-15 18:32:04 -0500

    add copyright

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -220,7 +220,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p><a href="http://www.sigmadev.ca">SIGMA Development</a> 2017</p>
+                    <p>Copyright © 2017 by <a href="http://www.sigmadev.ca">SIGMA Development</a> 2017</p>
                 </div>
             </div>
         </footer>
